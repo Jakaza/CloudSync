9344deb85fcc9fe150acd75e71acf43f664241bc
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-18 15:42:11 +0200

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export function generateUniqueId(): string {
 	const subset = "123456789abcdefghijklmnopqrstuvwxyz";
-	const length = 5;
+	const length = 8;
 	let id:string = "";
 	for (let i = 0; i < length; i++) {
 		id += subset[Math.floor(Math.random() * subset.length)];
