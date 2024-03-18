162468886218684906e4f42364f5f5591d72aa8c
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-18 15:18:06 +0200

    Create index.ts
    generateUniqueId func

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export function generateUniqueId(): string {
+	const subset = "123456789abcdefghijklmnopqrstuvwxyz";
+	const length = 5;
+	let id:string = "";
+	for (let i = 0; i < length; i++) {
+		id += subset[Math.floor(Math.random() * subset.length)];
+	}
+	return id;
+}
