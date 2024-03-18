cea5c77aa3681d1d339824146becfdb00e05f4ed
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-18 15:45:30 +0200

    get All Files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 export function generateUniqueId(): string {
 	const subset = "123456789abcdefghijklmnopqrstuvwxyz";
 	const length = 8;
@@ -6,4 +9,20 @@ export function generateUniqueId(): string {
 		id += subset[Math.floor(Math.random() * subset.length)];
 	}
 	return id;
+}
+
+export const getAllFiles = (folderPath: string) => {
+    let response: string[] = [];
+
+    const allFilesAndFolders = fs.readdirSync(folderPath);
+	
+	allFilesAndFolders.forEach(file => {
+        const fullFilePath = path.join(folderPath, file);
+        if (fs.statSync(fullFilePath).isDirectory()) {
+            response = response.concat(getAllFiles(fullFilePath))
+        } else {
+            response.push(fullFilePath);
+        }
+    });
+    return response;
 }
