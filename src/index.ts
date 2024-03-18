df229c8b739b1a6f7630048c25c27c797bb47f62
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-18 16:35:38 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import path from "path";
 import simpleGit from 'simple-git';
-import { generateUniqueId } from './utils';
+import { generateUniqueId, getAllFiles } from './utils';
 
 const app = express();
 app.use(cors());
@@ -12,12 +13,16 @@ app.get('/testing', async (req, res) => {
   res.json({ "testing": "It Works" });
 });
 
+
 app.post('/deploy', async (req, res) => {
 
     const {repo} = req.body;
-    await simpleGit().clone(repo, `output/${generateUniqueId()}`)
-    res.json(repo);
+    const id = generateUniqueId();
+    await simpleGit().clone(repo, path.join(__dirname, `output/${id}`))
 
+    const files = getAllFiles(path.join(__dirname, `output/${id}`))
+    console.log(files);
+    res.json(repo);
   });
 
 const server = http.createServer(app);
